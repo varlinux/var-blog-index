02a5f6848b72d5f97fa95b64899b5e2724424fa7
varlinux <czm_1314@qq.com>
2021-03-03 18:41:41 +0800

    Init Project

---
+++ layouts/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-container id="container">
+        <Header/>
+        <app-main/>
+        <el-footer>
+          <Footer/>
+        </el-footer>
+      </el-container>
+      <!--侧边导航栏-->
+      <Aside v-show="sidebar"/>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import Aside from './components/Aside'
+  import Footer from './components/Footer'
+  import AppMain from "./AppMain";
+  import {mapState, mapActions} from "vuex";
+
+  export default {
+    data() {
+      return {
+        mainStatus: 'articleList'
+      }
+    },
+    computed: {
+      ...mapState({
+        sidebar: state => state.app.sidebar,
+      })
+    },
+    components: {
+      Header,
+      Aside,
+      Footer,
+      AppMain
+    },
+    created() {
+      document.body.clientWidth < 960 ? this.$store.dispatch('toggleDevice', 'mobile') :
+          this.$store.dispatch('toggleDevice', 'desktop')
+    },
+    methods: {
+      ...mapActions([
+        'toggleDevice'
+      ])
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  @import "~@/assets/style/layout/index.sass"
+</style>
